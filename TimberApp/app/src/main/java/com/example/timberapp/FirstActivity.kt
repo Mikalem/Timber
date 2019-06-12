edbedc81c8300e71ddd8a08496b0c35ab17d1aae
Keara Leibovitz <kearatar@gmail.com>
2019-06-12 12:40:25 -0400

    Rename MainActivity to FirstActivity

--- TimberApp/app/src/main/java/com/example/timberapp/FirstActivity.kt
+++ TimberApp/app/src/main/java/com/example/timberapp/FirstActivity.kt
@@ -3,10 +3,10 @@ package com.example.timberapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_first)
     }
 }
